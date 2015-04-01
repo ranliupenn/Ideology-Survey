909cf8d15e798fe86072b7e6877d98084ffcd9ee
Ran <ranliupenn@gmail.com>
2015-04-01 01:14:58 -0400

    Update 2014data_recode.R
    updated recoding for demographic variables

--- 2014data_recode.R
+++ 2014data_recode.R
@@ -21,5 +21,11 @@ data[data=="强烈反对"]<-1
 data[data=="反对"]<-2
 data[data=="同意"]<-3
 data[data=="强烈同意"]<-4
+data$female[data$sex=="F"]<- 1
+data$female[data$sex=="M"]<- 0
+data$income<-recode(data$income,"'0-25k'=1;'25k-50k'=2;'50k-75k'=3;
+                    '75k-100k'=4;'100k-150k'=5;'150k-300k'=6;'300k+'=7")
+data$education<- recode(data$education, "'初中及以下'=1;'高中'=2;
+                        '大学'=3;'研究生及以上'=4")
 View(data)
 
